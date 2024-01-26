7e5a664828d822a412cc5ef6b44228c6d6efce84
IvanR <ivan.ramirezf@gmail.com>
2024-01-27 06:37:16 +1000

    Added Logo component
    - Added Logo component and CSS module
    - Added a link to Logo component in PageNav or navigation bar

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
